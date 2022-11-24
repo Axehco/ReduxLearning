8ad7ec458b94350ea9ae54697cf11d07e3ccccf1
Axehco <mbozhong@outlook.com>
2022-11-24 21:49:43 +0800

    102_求和案例_异步action版

--- src/Components/Count/index.jsx
+++ src/Components/Count/index.jsx
@@ -1,38 +1,36 @@
 import React, { Component } from 'react'
 //引入store，用于获取redux中保存状态
 import store from '../../redux/store'
-import {createIncrementAction, createDecrementAction} from '../../redux/count_action'
+import { createIncrementAction, createDecrementAction, createIncrementAsyncAction } from '../../redux/count_action'
 
 export default class Count extends Component {
 
   /* componentDidMount(){
-		//检测redux中状态的变化，只要变化，就调用render
-		store.subscribe(()=>{
-			this.setState({})
-		})
-	} */
+    //检测redux中状态的变化，只要变化，就调用render
+    store.subscribe(()=>{
+      this.setState({})
+    })
+  } */
 
   increment = () => {
-    const {value} = this.selectNumber
+    const { value } = this.selectNumber
     store.dispatch(createIncrementAction(value * 1))
   }
 
   decrement = () => {
-    const {value} = this.selectNumber
+    const { value } = this.selectNumber
     store.dispatch(createDecrementAction(value * 1))
   }
 
   incrementIfOdd = () => {
-    const {value} = this.selectNumber
+    const { value } = this.selectNumber
     const count = store.getState()
     if (count % 2 === 1) store.dispatch(createIncrementAction(value * 1))
   }
 
   incrementAsync = () => {
-    const {value} = this.selectNumber
-    setTimeout(() => {
-      store.dispatch(createIncrementAction(value * 1))
-    }, 3000)
+    const { value } = this.selectNumber
+    store.dispatch(createIncrementAsyncAction(value * 1, 3000))
   }
 
   render() {
