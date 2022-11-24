921eea22c71c14a3212596fc8965d20f580de2fd
Axehco <mbozhong@outlook.com>
2022-11-24 17:13:07 +0800

    100_求和案例_redux精简版

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Count from './Components/Count'
+import Count from './components/Count'
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ ReactDOM.render(<App />, document.getElementById('root')) */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import store from './redux/store'
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
+store.subscribe(() => {
+  root.render(<App />);
+})
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />)
