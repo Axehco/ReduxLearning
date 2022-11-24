e2d897b780fce772442b3ee1fc53a7dc1dba5b0d
Axehco <mbozhong@outlook.com>
2022-11-24 16:04:36 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/* import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root')) */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
