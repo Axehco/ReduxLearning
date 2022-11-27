b4c5708c2ac6f15f8edb295d0ae4d2132686052f
Axehco <mbozhong@outlook.com>
2022-11-27 20:38:57 +0800

    113_redux开发者工具

--- src/redux/reducers/person.js
+++ src/redux/reducers/person.js
@@ -5,6 +5,7 @@ export default function personReducer(preState = initState, action) {
   const { type, data } = action
   switch (type) {
     case ADD_PERSON:
+      // preState.unshift(data) // 此处不可以这样写，这样会导致preState被改写了，personReducer就不是纯函数了。
       return [data, ...preState]
     default:
       return preState
